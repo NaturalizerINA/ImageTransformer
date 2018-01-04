27bf66267f99117822ea04093042b0f8216b6c88
mamed <rahmad@codelabs.co.id>
2018-01-04 11:58:54 +0700

    Adding Source Code

---
+++ app/src/main/java/com/mukminullah/imagetransformer/MainActivity.java
@@ -0,0 +1,51 @@
+package com.mukminullah.imagetransformer;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imgLoad = (ImageView) findViewById(R.id.img_load);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.pb);
+
+        Glide.with(MainActivity.this)
+                .load("https://wolfpaulus.com/wp-content/uploads/2010/08/android.png")
+                .apply(bitmapTransform(new CircleCrop()))
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(MainActivity.this, "Something wrong: " + e, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                }).
+                into(imgLoad);
+
+    }
+}
